fc10aaa50f53d554e56524f7ee2f8bdc30520080
PKKostov18 <denyovg@gmail.com>
2026-04-04 18:09:04 +0300

    feat(discovery-server): initialize Netflix Eureka service registry

--- discovery-server/src/test/java/com/university/discovery_server/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/com/university/discovery_server/DiscoveryServerApplicationTests.java
@@ -1,12 +1,10 @@
-package com.university.user;
+package com.university.discovery_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
-class UserServiceApplicationTests {
+class DiscoveryServerApplicationTests {
 
 	@Test
 	void contextLoads() {
